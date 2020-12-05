84f0b28cf25da57e3af40b524e20d75f310ba341
Hendrik Legge <admin@themepoint.de>
2020-12-05 14:13:24 +0100

    Initial commit

---
+++ jquery.myFunction.js
@@ -0,0 +1,11 @@
+(function($){
+    $.fn.extend({
+        myFunction: function (options) {
+            options = $.extend({}, options);
+
+            // Code runs here...
+
+            return this;
+        }
+    });
+})(jQuery);
